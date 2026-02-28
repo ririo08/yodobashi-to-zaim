d86e4774f402dccc55bff1370dd88287bc62b087
ririo08 <7778943+ririo08@users.noreply.github.com>
2026-02-28 19:33:31 +0900

    chore: Github Pages settings

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS === 'true'
+    ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1] || ''}/`
+    : '/',
   plugins: [
     vue(),
     vueDevTools(),
